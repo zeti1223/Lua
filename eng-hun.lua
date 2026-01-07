47a39b1cb11bc4bd0a5d0e4cae92e2c97688845c
Zeti_1223 <121749710+zeti1223@users.noreply.github.com>
2026-01-07 20:00:50 +0100

    update

--- eng-hun.lua
+++ eng-hun.lua
@@ -1,10 +1,17 @@
-local szotar = {
-    ["hello"] = "szia",
-    ["apple"] = "alma",
-    ["house"] = "ház",
-    ["cat"] = "macska",
-    ["water"] = "víz"
-}
+local szotar = {}
+local file = io.open("szotar.txt", "r")
+if not file then
+    print("Hiba: Nem sikerült megnyitni a szotar.txt fájlt!")
+    os.exit()
+end
+
+for line in file:lines() do
+    local angol, magyar = line:match("([^=]+)=(.+)")
+    if angol and magyar then
+        szotar[angol:match("^%s*(.-)%s*$")] = magyar
+    end
+end
+file:close()
 
 print("Angol-magyar szótár gyakorló. Írd be a fordítást!")
 
@@ -15,7 +22,16 @@ for angol, magyar in pairs(szotar) do
     osszesSzo = osszesSzo + 1
     io.write("Mi a magyar fordítása a(z) '" .. angol .. "' szónak? ")
     local valasz = io.read():lower():gsub("%s+", "")
-    if valasz == magyar then
+
+    local helyes = false
+    for szo in magyar:gmatch("([^,]+)") do
+        if valasz == szo:lower():gsub("%s+", "") then
+            helyes = true
+            break
+        end
+    end
+
+    if helyes then
         print("✅ Helyes!")
         pontszam = pontszam + 1
     else
